df88f3165ccdcfa55bb664ff91c98dd28a97f356
Shhhpoulpe <max.jobard@hotmail.fr>
2019-03-28 08:57:16 +0100

    First Commit

---
+++ GSB_service/GestionDate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_service
+{
+    class GestionDate
+    {
+        System.DateTime date = DateTime.Now;
+
+        public int getMonth()
+        {
+            return date.Month;
+        }
+
+        public int getLastMonth()
+        {
+            if (date.Month - 1 != 0)
+            {
+                return date.Month - 1;
+            }
+            else
+            {
+                return 12;
+            }
+        }
+
+        public int getDay()
+        {
+            return date.Day;
+        }
+
+        public int getYear()
+        {
+            if (getLastMonth() != 12)
+            {
+                return date.Year;
+            }
+            else
+            {
+                return date.Year - 1;
+            }
+
+        }
+    }
+}
---
+++ GSB_service/Service1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GSB_service
+{
+    public partial class Service1 : ServiceBase
+    {
+        private ConnexionSql BDD;
+        private GestionDate date;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.BDD = ConnexionSql.getInstance("localhost", "gsb", "root", "");
+
+            BDD.openConnection();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string mois = date.getYear().ToString() + date.getLastMonth().ToString("00");
+
+
+            if (date.getDay() >= 20)
+            {
+
+                MySqlCommand commande = BDD.reqExec("update fichefrais set idEtat = 'RB' where idEtat = 'CL' AND mois='" + mois + "' ");
+
+                commande.ExecuteNonQuery();
+
+            }
+
+            if (date.getDay() <= 10)
+            {
+
+                MySqlCommand commande = BDD.reqExec("update fichefrais set idEtat = 'CL' where mois= '" + mois + "' ");
+
+                commande.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
